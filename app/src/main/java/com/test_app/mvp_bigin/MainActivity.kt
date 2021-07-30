caca3e5d85909c0e287a30341f14fc2bafb3e732
Rasel579 <Rasel579@gmail.com>
2021-07-30 23:31:34 +0500

    finished homework for lesson 1, added model with view and presenter for dividing methods for every view

---
+++ app/src/main/java/com/test_app/mvp_bigin/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.test_app.mvp_bigin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.test_app.mvp_bigin.databinding.ActivityMainBinding
+import com.test_app.mvp_bigin.presentation.Presenter
+import com.test_app.mvp_bigin.views.MainView
+
+class MainActivity : AppCompatActivity(), MainView {
+    private var binding : ActivityMainBinding ? = null
+    private val presenter = Presenter(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        initView()
+    }
+
+    private fun initView() {
+        binding?.btnCounter1?.setOnClickListener{presenter.firstBtnClicked()}
+        binding?.btnCounter2?.setOnClickListener{presenter.secondBtnClicked()}
+        binding?.btnCounter3?.setOnClickListener{presenter.thirdBtnClicked()}
+    }
+
+    override fun getFirstBtnValue(value: Int) {
+        binding?.btnCounter1?.text  = value.toString()
+    }
+
+    override fun getSecondBtnValue(value: Int) {
+        binding?.btnCounter2?.text  = value.toString()
+
+    }
+
+    override fun getThirdBtnValue(value: Int) {
+        binding?.btnCounter3?.text  = value.toString()
+    }
+}
---
+++ app/src/main/java/com/test_app/mvp_bigin/views/MainView.kt
@@ -0,0 +1,7 @@
+package com.test_app.mvp_bigin.views
+
+interface MainView {
+    fun getFirstBtnValue(value : Int)
+    fun getSecondBtnValue(value: Int)
+    fun getThirdBtnValue(value: Int)
+}
